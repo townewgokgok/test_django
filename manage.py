5004c178490dcd95f76a1a1ef9682b7694ca8f9e
k-furukawa <townewgokgok@gmail.com>
2016-11-02 17:39:51 +0900

    MySQL設定

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
+import pymysql
+pymysql.install_as_MySQLdb()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
